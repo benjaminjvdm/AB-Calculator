ccf14a0d9818b63e3e22f35e2a463570c3a4c62b
Benjamin (James) van der Merwe <97449931+benjaminjvdm@users.noreply.github.com>
2025-02-01 19:45:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,16 @@ with st.sidebar:
         response = requests.get(image_url)
         response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
         image = response.content
-        st.image(image, caption="Moon Benjee (문벤지)", use_column_width=True)
+        st.image(image, caption="Moon Benjee (문벤지)", use_container_width=True)
     except requests.exceptions.RequestException as e:
         st.error(f"Error loading image: {e}")  # Use st.error for better visibility
 
     st.markdown(
         """
-        This app was created by **Moon Benjee (문벤지)**. 
+        This app was Built with ❤️ by **Benjee(문벤지)**. 
         You can connect with me on: [LinkedIn](https://www.linkedin.com/in/benjaminjvdm/)
         """
     )
-
-    st.sidebar.markdown("---")
-    st.sidebar.text("Built with ❤️ by Benjee(문벤지)")
-
 
 st.title("A/B Test Calculator")
 
